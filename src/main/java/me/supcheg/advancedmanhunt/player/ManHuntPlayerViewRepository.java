c1e919993af8c7e51a55a01af910af3787dead1f
Pupcheg <66114027+Pupcheg@users.noreply.github.com>
2023-07-30 14:01:02 +0300

    ManHuntPlayerViewRepository cleanup

--- src/main/java/me/supcheg/advancedmanhunt/player/ManHuntPlayerViewRepository.java
+++ src/main/java/me/supcheg/advancedmanhunt/player/ManHuntPlayerViewRepository.java
@@ -4,16 +4,10 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.UnmodifiableView;
 
-import java.util.Collection;
 import java.util.UUID;
 
 public interface ManHuntPlayerViewRepository {
-    @NotNull
-    @UnmodifiableView
-    Collection<ManHuntPlayerView> getPlayers();
-
     @NotNull
     default ManHuntPlayerView get(@NotNull Player player) {
         return get(player.getUniqueId());
--- src/main/java/me/supcheg/advancedmanhunt/player/impl/DefaultManHuntPlayerViewRepository.java
+++ src/main/java/me/supcheg/advancedmanhunt/player/impl/DefaultManHuntPlayerViewRepository.java
@@ -6,32 +6,15 @@ import lombok.SneakyThrows;
 import me.supcheg.advancedmanhunt.player.ManHuntPlayerView;
 import me.supcheg.advancedmanhunt.player.ManHuntPlayerViewRepository;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.UnmodifiableView;
 
-import java.util.Collection;
-import java.util.Collections;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 public class DefaultManHuntPlayerViewRepository implements ManHuntPlayerViewRepository {
-
-    private final Cache<UUID, ManHuntPlayerView> uniqueId2view = CacheBuilder.newBuilder()
-            .weakValues()
-            .expireAfterWrite(20, TimeUnit.MINUTES)
-            .expireAfterAccess(20, TimeUnit.MINUTES)
-            .build();
-    private final Collection<ManHuntPlayerView> allViews = Collections.unmodifiableCollection(uniqueId2view.asMap().values());
-
-    @Override
-    @NotNull
-    @UnmodifiableView
-    public Collection<ManHuntPlayerView> getPlayers() {
-        return allViews;
-    }
+    private final Cache<UUID, ManHuntPlayerView> uniqueId2view = CacheBuilder.newBuilder().weakValues().build();
 
     @SneakyThrows
-    @Override
     @NotNull
+    @Override
     public ManHuntPlayerView get(@NotNull UUID uniqueId) {
         return uniqueId2view.get(uniqueId, () -> new DefaultManHuntPlayerView(uniqueId));
     }
