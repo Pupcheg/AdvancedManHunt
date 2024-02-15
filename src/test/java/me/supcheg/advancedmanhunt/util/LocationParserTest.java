b30b107fe07432f3669a8e22aa0e8f984fcc741f
Pupcheg <santos43241@gmail.com>
2024-02-15 16:36:05 +0300

    Added serialize ImmutableLocation test

--- src/test/java/me/supcheg/advancedmanhunt/util/LocationParserTest.java
+++ src/test/java/me/supcheg/advancedmanhunt/util/LocationParserTest.java
@@ -11,12 +11,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static me.supcheg.advancedmanhunt.util.LocationParser.parseLocation;
+import static me.supcheg.advancedmanhunt.util.LocationParser.serializeLocation;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LocationParserTest {
 
-    private World world;
+    World world;
 
     @BeforeEach
     void setup() {
@@ -30,17 +31,22 @@ class LocationParserTest {
     }
 
     @Test
-    void throwTest() {
+    void parseThrows() {
         assertThrows(Throwable.class, () -> parseLocation("wgk2w-254];"));
     }
 
     @Test
-    void spawnTest() {
+    void parseSpawnWithoutDirection() {
         assertParseResult(ImmutableLocation.copyOf(world.getSpawnLocation()), "world[spawn]");
     }
 
     @Test
-    void spawnWithDirectionTest() {
+    void serializeSpawnWithoutDirection() {
+        assertSerializeResult("world[spawn]", ImmutableLocation.copyOf(world.getSpawnLocation()));
+    }
+
+    @Test
+    void parseSpawnWithDirection() {
         Location spawnWithDirection = world.getSpawnLocation();
         spawnWithDirection.setYaw(180f);
         spawnWithDirection.setPitch(-25.5f);
@@ -49,17 +55,40 @@ class LocationParserTest {
     }
 
     @Test
-    void locationWithoutDirectionTest() {
+    void serializeSpawnWithDirection() {
+        Location spawnWithDirection = world.getSpawnLocation();
+        spawnWithDirection.setYaw(180f);
+        spawnWithDirection.setPitch(-25.5f);
+
+        assertSerializeResult("world[spawn, 180.0, -25.5]", ImmutableLocation.copyOf(spawnWithDirection));
+    }
+
+    @Test
+    void parseLocationWithoutDirection() {
         assertParseResult(new ImmutableLocation(world, 20.5, .33, -30, 0, 0), "world[20.5, .33, -30]");
     }
 
     @Test
-    void locationWithDirectionTest() {
+    void serializeLocationWithoutDirection() {
+        assertSerializeResult("world[20.5, 0.33, -30.0]", new ImmutableLocation(world, 20.5, .33, -30, 0, 0));
+    }
+
+    @Test
+    void parseLocationWithDirection() {
         assertParseResult(new ImmutableLocation(world, 10, 10, 10, .55f, -0.5f), "world[10, 10, 10, .55, -0.5]");
+    }
+
+    @Test
+    void serializeLocationWithDirection() {
+        assertSerializeResult("world[10.0, 10.0, 10.0, 0.55, -0.5]", new ImmutableLocation(world, 10, 10, 10, .55f, -0.5f));
     }
 
     private void assertParseResult(@NotNull ImmutableLocation expected, @NotNull String serialized) {
         assertEquals(expected, parseLocation(serialized));
+    }
+
+    private void assertSerializeResult(@NotNull String expected, @NotNull ImmutableLocation actual) {
+        assertEquals(expected, serializeLocation(actual));
     }
 
 }
