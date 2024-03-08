8f2240b8c6c8ea366d8c61414b9584a9a284e014
Pupcheg <santos43241@gmail.com>
2024-03-08 22:48:28 +0300

    LogicDelegates compilation fix

--- src/main/java/me/supcheg/advancedmanhunt/gui/impl/common/logic/LogicDelegates.java
+++ src/main/java/me/supcheg/advancedmanhunt/gui/impl/common/logic/LogicDelegates.java
@@ -4,14 +4,14 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.invoke.MethodHandle;
 import java.util.Objects;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class LogicDelegates {
     @NotNull
     public static LogicDelegate staticDelegate() {
-        return MethodHandle::invoke;
+        // IntelliJ is wrong here. Only in this form can the compilation be possible.
+        return (handle, arg) -> handle.invoke(arg);
     }
 
     @NotNull
