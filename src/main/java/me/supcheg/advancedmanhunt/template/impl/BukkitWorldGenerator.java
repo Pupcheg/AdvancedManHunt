c050fd039f08edd158df778fecda690de37f8295
Pupcheg <santos43241@gmail.com>
2024-03-09 23:45:41 +0300

    Made BukkitWorldGenerator sync

--- src/main/java/me/supcheg/advancedmanhunt/template/impl/BukkitWorldGenerator.java
+++ src/main/java/me/supcheg/advancedmanhunt/template/impl/BukkitWorldGenerator.java
@@ -1,23 +1,19 @@
 package me.supcheg.advancedmanhunt.template.impl;
 
+import me.supcheg.advancedmanhunt.coord.Coord;
 import me.supcheg.advancedmanhunt.coord.CoordRangeIterator;
 import me.supcheg.advancedmanhunt.coord.Coords;
-import me.supcheg.advancedmanhunt.coord.Coord;
 import me.supcheg.advancedmanhunt.template.WorldGenerator;
 import org.bukkit.World;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.concurrent.CompletableFuture;
-
 public class BukkitWorldGenerator implements WorldGenerator {
     @Override
     public void generate(@NotNull World world, int radius, @NotNull Runnable afterGeneration) {
-        CompletableFuture.runAsync(() -> {
-            CoordRangeIterator it = Coords.iterateRangeInclusive(Coord.coordSameXZ(-radius / 8), Coord.coordSameXZ(radius / 8));
-            for (; it.hasNext(); it.next()) {
-                world.loadChunk(it.getX(), it.getZ());
-            }
-            afterGeneration.run();
-        });
+        CoordRangeIterator it = Coords.iterateRangeInclusive(Coord.coordSameXZ(-radius / 8), Coord.coordSameXZ(radius / 8));
+        for (; it.hasNext(); it.next()) {
+            world.loadChunk(it.getX(), it.getZ());
+        }
+        afterGeneration.run();
     }
 }
