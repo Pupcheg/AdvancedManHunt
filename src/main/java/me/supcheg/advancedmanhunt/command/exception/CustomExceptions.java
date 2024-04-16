92b001043846b62c767d3719342eeeabaefc3414
Pupcheg <santos43241@gmail.com>
2024-04-16 20:28:35 +0300

    Removed unused exception type from CustomExceptions

--- src/main/java/me/supcheg/advancedmanhunt/command/exception/CustomExceptions.java
+++ src/main/java/me/supcheg/advancedmanhunt/command/exception/CustomExceptions.java
@@ -1,10 +1,8 @@
 package me.supcheg.advancedmanhunt.command.exception;
 
 import com.mojang.brigadier.LiteralMessage;
-import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 
 public class CustomExceptions {
     public static final SimpleCommandExceptionType ACCESS_DENIED = new SimpleCommandExceptionType(new LiteralMessage("Access denied"));
-    public static final DynamicCommandExceptionType NULL = new DynamicCommandExceptionType(a -> new LiteralMessage(a + " is null"));
 }
