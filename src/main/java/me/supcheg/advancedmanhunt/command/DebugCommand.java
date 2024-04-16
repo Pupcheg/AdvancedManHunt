4970e39c424d29df85d22151e42fe8f3f5938745
Pupcheg <santos43241@gmail.com>
2024-04-16 20:27:47 +0300

    Removed try-catches from DebugCommand

--- src/main/java/me/supcheg/advancedmanhunt/command/DebugCommand.java
+++ src/main/java/me/supcheg/advancedmanhunt/command/DebugCommand.java
@@ -5,6 +5,7 @@ import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.supcheg.advancedmanhunt.command.argument.AdvancedGuiArgument;
@@ -67,49 +68,37 @@ public class DebugCommand implements BukkitBrigadierCommand {
     }
 
     @SuppressWarnings("SameReturnValue") // command entrypoint
-    private int openGui(@NotNull CommandContext<BukkitBrigadierCommandSource> ctx) {
-        try {
-            guiArgument.getGui(ctx, KEY).open(getPlayer(ctx));
-        } catch (Throwable thr) {
-            log.error("", thr);
-        }
+    private int openGui(@NotNull CommandContext<BukkitBrigadierCommandSource> ctx) throws CommandSyntaxException {
+        guiArgument.getGui(ctx, KEY).open(getPlayer(ctx));
         return Command.SINGLE_SUCCESS;
     }
 
     @SuppressWarnings("SameReturnValue") // command entrypoint
-    private int loadTemplate(@NotNull CommandContext<BukkitBrigadierCommandSource> ctx) {
-        try {
-            Template template = templateArgument.getTemplate(ctx, KEY);
+    private int loadTemplate(@NotNull CommandContext<BukkitBrigadierCommandSource> ctx) throws CommandSyntaxException {
+        Template template = templateArgument.getTemplate(ctx, KEY);
 
-            WorldReference reference = WorldReference.of("amh_rw-3");
-            GameRegion region = new GameRegion(reference, Coord.coordSameXZ(32), Coord.coordSameXZ(64));
+        WorldReference reference = WorldReference.of("amh_rw-3");
+        GameRegion region = new GameRegion(reference, Coord.coordSameXZ(32), Coord.coordSameXZ(64));
 
-            templateService.loadTemplate(region, template).join();
+        templateService.loadTemplate(region, template).join();
 
-            Location center = region.getCenterBlock().asLocation(reference.getWorld(), 80);
-
-            getPlayer(ctx).teleport(center);
-        } catch (Throwable thr) {
-            log.error("", thr);
-        }
+        Location center = region.getCenterBlock().asLocation(reference.getWorld(), 80);
+        getPlayer(ctx).teleport(center);
 
         return Command.SINGLE_SUCCESS;
     }
 
     @SuppressWarnings("SameReturnValue") // command entrypoint
     private int fastGame(@NotNull CommandContext<BukkitBrigadierCommandSource> ctx) {
-        try {
-            Iterator<? extends Player> onlinePlayers = Bukkit.getOnlinePlayers().iterator();
-            UUID player1 = onlinePlayers.next().getUniqueId();
-            UUID player2 = onlinePlayers.next().getUniqueId();
+        Iterator<? extends Player> onlinePlayers = Bukkit.getOnlinePlayers().iterator();
+        UUID player1 = onlinePlayers.next().getUniqueId();
+        UUID player2 = onlinePlayers.next().getUniqueId();
 
-            ManHuntGame game = gameService.createGame(player1);
-            game.addMember(player1);
-            game.addMember(player2);
-            gameService.start(game);
-        } catch (Throwable thr) {
-            log.error("", thr);
-        }
+        ManHuntGame game = gameService.createGame(player1);
+        game.addMember(player1);
+        game.addMember(player2);
+        gameService.start(game);
+
         return Command.SINGLE_SUCCESS;
     }
 }
