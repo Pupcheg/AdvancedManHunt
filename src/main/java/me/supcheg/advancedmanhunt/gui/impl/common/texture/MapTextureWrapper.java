cb3a41cc45aae84caa76b21339628841637b0ad0
Pupcheg <santos43241@gmail.com>
2024-04-18 21:39:44 +0300

    Removed logger from MapTextureWrapper

--- src/main/java/me/supcheg/advancedmanhunt/gui/impl/common/texture/MapTextureWrapper.java
+++ src/main/java/me/supcheg/advancedmanhunt/gui/impl/common/texture/MapTextureWrapper.java
@@ -1,13 +1,11 @@
 package me.supcheg.advancedmanhunt.gui.impl.common.texture;
 
-import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-@Slf4j
 public class MapTextureWrapper implements TextureWrapper {
     private final Map<String, PaperItemTexture> paperItems;
     private final Map<String, ComponentGuiTexture> guiComponents;
@@ -37,21 +35,17 @@ public class MapTextureWrapper implements TextureWrapper {
 
     public void addPaperItemTexture(@NotNull PaperItemTexture texture) {
         paperItems.put(texture.getPath(), texture);
-        log.debug("Added: {}", texture);
     }
 
     public void removePaperItemTexture(@NotNull String resourcePath) {
         paperItems.remove(resourcePath);
-        log.debug("Removed: {}", resourcePath);
     }
 
     public void addComponentGuiTexture(@NotNull ComponentGuiTexture texture) {
         guiComponents.put(texture.getPath(), texture);
-        log.debug("Added: {}", texture);
     }
 
     public void removeComponentGuiTexture(@NotNull String resourcePath) {
         guiComponents.remove(resourcePath);
-        log.debug("Removed: {}", resourcePath);
     }
 }
